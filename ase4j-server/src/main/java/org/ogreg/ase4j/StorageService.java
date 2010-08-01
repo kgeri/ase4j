5be5bd437e51065a017ba7ad18dac25829379a59
Gergely Kiss <kiss.gergely@gmail.com>
2010-08-01 11:31:30 +0200

    storage service improvements

--- ase4j-server/src/main/java/org/ogreg/ase4j/StorageService.java
+++ ase4j-server/src/main/java/org/ogreg/ase4j/StorageService.java
@@ -3,7 +3,12 @@ package org.ogreg.ase4j;
 import gnu.cajo.invoke.Remote;
 import gnu.cajo.utils.ItemServer;
 
+import java.io.Closeable;
 import java.io.File;
+import java.io.Flushable;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Map.Entry;
 
 import org.ogreg.ostore.ObjectStore;
@@ -19,9 +24,16 @@ public class StorageService {
 	private static final Logger log = LoggerFactory.getLogger(StorageService.class);
 
 	private AssociationStoreManager storeManager;
+	private final Map<String, AssociationStore<?, ?>> associationStores = new HashMap<String, AssociationStore<?, ?>>();
+
+	private boolean running = false;
 
 	public void start() throws Exception {
-		log.info("Starting storage...");
+		running = true;
+
+		log.info("Starting up storage...");
+		// TODO PerformanceTimer - CJC maybe?
+		long before = System.currentTimeMillis();
 
 		// Reading configuration
 		String dataPath = System.getProperty("dataDir", "data");
@@ -31,55 +43,156 @@ public class StorageService {
 		File dataDir = new File(dataPath);
 		File schemaFile = new File(schemaPath);
 
+		// TODO check permissions?
+		log.debug("Using data dir: {}", dataDir);
+
 		// Initializing Cajo
 		Remote.config(null, rmiPort, null, 0);
 
-		// Initializing storage manager
-		// TODO Load multiple configs
+		// Initializing storage
+		// TODO Load multiple configs?
 		storeManager = new AssociationStoreManager();
 		storeManager.add(schemaFile);
 		storeManager.setDataDir(dataDir);
 
 		// Publishing association storage
-		log.info("Configuring association storage");
+		log.debug("Configuring association storage");
+		int assocStores = 0;
+
 		for (String id : storeManager.getConfiguredStores()) {
-			publish(id);
+			String serviceLoc = "assocs/" + id;
+
+			try {
+				@SuppressWarnings("rawtypes")
+				AssociationStore store = storeManager.createStore(id);
+				ItemServer.bind(store, serviceLoc);
+				associationStores.put(id, store);
+				assocStores++;
+				log.debug("Successfully initialized assoc store at: {}", serviceLoc);
+			} catch (Exception e) {
+				log.error("Failed to initialize assoc store: {} ({})", id, e.getLocalizedMessage());
+				log.debug("Failure trace", e);
+			}
 		}
+
+		log.info("Initialized {}/{} assoc stores successfully", assocStores, storeManager
+				.getConfiguredStores().size());
 
 		// Publishing object storage
-		log.info("Configuring object storage");
+		log.debug("Configuring object storage");
+		int objectStores = 0;
+
+		for (Entry<String, ObjectStore<?>> en : storeManager.getObjectStores().entrySet()) {
+			String id = en.getKey();
+			String serviceLoc = "objects/" + id;
+
+			try {
+				ItemServer.bind(en.getValue(), serviceLoc);
+				objectStores++;
+				log.debug("Successfully initialized object store at: {}", serviceLoc);
+			} catch (Exception e) {
+				log.error("Failed to initialize object store: {} ({})", id, e.getLocalizedMessage());
+				log.debug("Failure trace", e);
+			}
+		}
+
+		// TODO PerformanceTimer
+		long time = System.currentTimeMillis() - before;
+		log.info("Initialized {}/{} object stores successfully", objectStores, storeManager
+				.getObjectStores().size());
+
+		log.info("Startup completed in {} ms, waiting for requests", time);
+
+		// Adding shutdown hook (Ctrl+C)
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			@Override
+			public void run() {
+				shutdown();
+			}
+		});
+
+		// Starting console
+		Thread console = new Thread("Console") {
+			@Override
+			public void run() {
+				try {
+					System.out.println("Enter 'q' or Ctrl+C to quit");
+					while (true) {
+						int b = System.in.read();
+						if (b < 0 || b == 'q') {
+							shutdown();
+						}
+					}
+				} catch (IOException e) {
+					log.error("Failed to read from System.in");
+				}
+			}
+		};
+		console.setDaemon(true);
+		console.start();
+
+		while (running) {
+			Thread.sleep(100);
+		}
+	}
+
+	public synchronized void shutdown() {
+		if (!running) {
+			return;
+		}
+
+		// TODO PerformanceTimer
+		long before = System.currentTimeMillis();
+
+		log.debug("Shutting down assoc storage");
+		for (Entry<String, AssociationStore<?, ?>> e : associationStores.entrySet()) {
+			String id = e.getKey();
+			AssociationStore<?, ?> store = e.getValue();
+
+			shutdownStore(id, store);
+		}
+
+		log.debug("Shutting down object storage");
 		for (Entry<String, ObjectStore<?>> e : storeManager.getObjectStores().entrySet()) {
-			publish(e.getKey(), e.getValue());
+			String id = e.getKey();
+			ObjectStore<?> store = e.getValue();
+
+			shutdownStore(id, store);
 		}
 
-		log.info("Storage startup complete, waiting for requests");
+		// TODO PerformanceTimer
+		long time = System.currentTimeMillis() - before;
+		log.info("Shutdown completed in {} ms. Bye!", time);
+		running = false;
 	}
 
-	private void publish(String assocStoreId) {
+	void shutdownStore(String id, Object store) {
 		try {
-			log.debug("Configuring assoc store: {}", assocStoreId);
-			@SuppressWarnings("rawtypes")
-			AssociationStore store = storeManager.createStore(assocStoreId);
-			ItemServer.bind(store, "assocs/" + assocStoreId);
+			if (store instanceof Flushable) {
+				((Flushable) store).flush();
+			}
+			log.debug("Successfully flushed storage: {}", id);
 		} catch (Exception e) {
-			log.error("Failed to configure assoc store: {} ({})", assocStoreId,
-					e.getLocalizedMessage());
+			log.error("Failed to flush: {} ({}), trying to close", id);
+			log.debug("Failure trace", e);
+		}
+
+		try {
+			if (store instanceof Closeable) {
+				((Closeable) store).close();
+			}
+			log.debug("Successfully closed storage: {}", id);
+		} catch (Exception e) {
+			log.error("Failed to close: {} ({})", id);
 			log.debug("Failure trace", e);
 		}
 	}
 
-	private void publish(String objectStoreId, ObjectStore<?> store) {
+	public static void main(String[] args) {
 		try {
-			log.debug("Configuring object store: {}", objectStoreId);
-			ItemServer.bind(store, "objects/" + objectStoreId);
-		} catch (Exception e) {
-			log.error("Failed to configure object store: {} ({})", objectStoreId,
-					e.getLocalizedMessage());
-			log.debug("Failure trace", e);
+			new StorageService().start();
+		} catch (Throwable e) {
+			log.error("Unexpected storage failure", e);
 		}
-	}
-
-	public static void main(String[] args) throws Exception {
-		new StorageService().start();
 	}
 }
