3f8770c99b6ad9d397960bdc23bce0724e5276f0
Kiss Gergely <kiss.gergely@iqsys.hu>
2010-09-07 01:59:12 +0200

    groupedparams op parameter

--- ase4j-client/src/main/java/org/ogreg/ase4j/GroupedParams.java
+++ ase4j-client/src/main/java/org/ogreg/ase4j/GroupedParams.java
@@ -1,72 +1,80 @@
 package org.ogreg.ase4j;
 
+import org.ogreg.ase4j.AssociationStore.Operation;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
+
 /**
  * Grouped association parameters for storing and querying.
- * <p>
- * Used to specify which association stores need to be accessed and with what
- * multipliers.
- * </p>
- * 
- * @author Gergely Kiss
+ *
+ * <p>Used to specify which association stores need to be accessed and with what multipliers.</p>
+ *
+ * @author  Gergely Kiss
  */
 public class GroupedParams extends Params {
-	private static final long serialVersionUID = 1158867616552676901L;
+    private static final long serialVersionUID = 1158867616552676901L;
 
-	/** The multipliers used when accessing the stores, keyed by the store ids. */
-	private Map<String, Float> multipliers = new HashMap<String, Float>();
+    /** The multipliers used when accessing the stores, keyed by the store ids. */
+    private Map<String, Float> multipliers = new HashMap<String, Float>();
 
-	public GroupedParams(String id, float multiplier) {
-		multipliers.put(id, multiplier);
-	}
+    public GroupedParams(String id, float multiplier) {
+        this(id, multiplier, Operation.AVG);
+    }
 
-	/**
-	 * Sets the <code>multiplier</code> for <code>storeId</code>.
-	 * 
-	 * @param storeId
-	 * @param multiplier
-	 * @return This instance for method chaining
-	 */
-	public GroupedParams set(String storeId, float multiplier) {
-		this.multipliers.put(storeId, Float.valueOf(multiplier));
+    public GroupedParams(String id, float multiplier, Operation op) {
+        super(op);
+        multipliers.put(id, multiplier);
+    }
 
-		return this;
-	}
+    /**
+     * Sets the <code>multiplier</code> for <code>storeId</code>.
+     *
+     * @param   storeId
+     * @param   multiplier
+     *
+     * @return  This instance for method chaining
+     */
+    public GroupedParams set(String storeId, float multiplier) {
+        this.multipliers.put(storeId, Float.valueOf(multiplier));
 
-	/**
-	 * Returns the group identifiers to access.
-	 * 
-	 * @return
-	 */
-	public Set<String> getGroups() {
-		return multipliers.keySet();
-	}
+        return this;
+    }
 
-	/**
-	 * Returns the group multipliers keyed by their group ids.
-	 * 
-	 * @return
-	 */
-	public Map<String, Float> getMultipliers() {
-		return multipliers;
-	}
+    /**
+     * Returns the group identifiers to access.
+     *
+     * @return
+     */
+    public Set<String> getGroups() {
+        return multipliers.keySet();
+    }
 
-	public static GroupedParams ensureNotNull(Params params) {
-		if (params == null) {
-			throw new IllegalArgumentException(
-					"Operations on grouped AssociationStores must specify a GroupedParams parameter which must not be null",
-					null);
-		}
+    /**
+     * Returns the group multipliers keyed by their group ids.
+     *
+     * @return
+     */
+    public Map<String, Float> getMultipliers() {
+        return multipliers;
+    }
 
-		try {
-			return (GroupedParams) params;
-		} catch (ClassCastException e) {
-			throw new IllegalArgumentException(
-					"Operations on grouped AssociationStores must specify a GroupedParams parameter",
-					e);
-		}
-	}
+    public static GroupedParams ensureNotNull(Params params) {
+
+        if (params == null) {
+            throw new IllegalArgumentException(
+                "Operations on grouped AssociationStores must specify a GroupedParams parameter which must not be null",
+                null);
+        }
+
+        try {
+            return (GroupedParams) params;
+        } catch (ClassCastException e) {
+            throw new IllegalArgumentException(
+                "Operations on grouped AssociationStores must specify a GroupedParams parameter",
+                e);
+        }
+    }
 }
--- ase4j-client/src/main/java/org/ogreg/ase4j/Params.java
+++ ase4j-client/src/main/java/org/ogreg/ase4j/Params.java
@@ -1,34 +1,33 @@
 package org.ogreg.ase4j;
 
+import org.ogreg.ase4j.AssociationStore.Operation;
+
 import java.io.Serializable;
 
-import org.ogreg.ase4j.AssociationStore.Operation;
 
 /**
  * Association parameters.
- * <p>
- * Association parameters specify the conditions for adding or querying
- * associations.
- * </p>
- * 
- * @author Gergely Kiss
+ *
+ * <p>Association parameters specify the conditions for adding or querying associations.</p>
+ *
+ * @author  Gergely Kiss
  */
 public class Params implements Serializable {
-	private static final long serialVersionUID = 1090624754798154827L;
-	private static final Params DEFAULT_PARAMS = new Params();
+    private static final long serialVersionUID = 1090624754798154827L;
+    private static final Params DEFAULT_PARAMS = new Params();
 
-	/** The operation to use when adding associations. */
-	public final AssociationStore.Operation op;
+    /** The operation to use when adding associations. */
+    public final Operation op;
 
-	public Params() {
-		this(Operation.AVG);
-	}
+    public Params() {
+        this(Operation.AVG);
+    }
 
-	public Params(AssociationStore.Operation op) {
-		this.op = op;
-	}
+    public Params(Operation op) {
+        this.op = op;
+    }
 
-	public static Params ensureNotNull(Params params) {
-		return params == null ? DEFAULT_PARAMS : params;
-	}
+    public static Params ensureNotNull(Params params) {
+        return (params == null) ? DEFAULT_PARAMS : params;
+    }
 }
